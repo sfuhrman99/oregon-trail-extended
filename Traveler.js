e56477c1d1065870ba86d7764f5c74b899f97210
Shane Fuhrman <sfuhrman99@gmail.com>
2020-07-17 16:50:56 -0400

    oregon trail extended

--- Traveler.js
+++ Traveler.js
@@ -9,9 +9,57 @@ class Traveler {
         this.food += 2
     }   
     eat () {
-        this.food -= 1
-        if (this.food < 1) {
+        if (this.food <= 0) {
             this.isHealthy = false
+        }
+        if(this.food > 0){
+            this.food -= 1
+
         }
     }   
 }
+
+class Doctor extends Traveler {
+    constructor (name){
+        super(name)
+        this.traveler = 0
+    }
+    heal(traveler){
+        if(traveler.isHealthy === false){
+            traveler.isHealthy = true
+        }
+        return this
+    }
+
+}
+
+class Hunter extends Traveler{
+    constructor(name){
+        super(name)
+        this.food = 2
+        }
+    
+        hunt(){
+            this.food += 5
+        }
+    
+        eat(){
+            if(this.food < 2){
+                this.food = 0
+                this.isHealthy = false
+            }
+            else {
+                this.food -= 2
+            }
+        }
+    
+        giveFood(traveler, numOfFoodUnits){
+            
+                if(this.food > numOfFoodUnits){
+                    traveler.food += numOfFoodUnits
+                    this.food -= numOfFoodUnits
+    
+                }
+            
+        }
+    }
